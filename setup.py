1d29ad3528c834a7ade717c0246d0e2be777db93
Paul Vergeev <paulvergeev@gmail.com>
2018-01-30 14:16:54 +0300

    Fix the bug with requirements not meing installed

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 import os
+from os.path import join, dirname
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+def load_readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+        return readme.read()
+
+
+def load_requirements():
+    return open(join(dirname(__file__), 'requirements.txt')).readlines()
+
 
 setup(
     name='django-social-pill',
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
-    license='MIT License',  # example license
+    license='MIT License',
     description='A Django app to speed up social auth adoption.',
-    long_description=README,
+    long_description=load_readme(),
+    install_requires=load_requirements()
 )
