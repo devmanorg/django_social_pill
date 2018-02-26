75861c180c833eb61f027cc389687db3548b6756
Paul Vergeev <paulvergeev@gmail.com>
2018-02-26 16:05:56 +0300

    Add the Telegram profile link button

--- django_social_pill/templatetags/social_link_buttons.py
+++ django_social_pill/templatetags/social_link_buttons.py
@@ -24,3 +24,7 @@ def show_facebook_link_button(user):
 def show_twitter_link_button(user):
     return {'twitter_url': utils.get_twitter_url(user)}
 
+
+@register.inclusion_tag('django_social_pill/telegram_link_button.html')
+def show_telegram_link_button(user):
+    return {'telegram_url': utils.get_telegram_url(user)}
