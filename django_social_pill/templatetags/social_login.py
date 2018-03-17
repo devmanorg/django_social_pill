40afd49849c75e799d623179cdce73efb2406c1e
melevir <melevir@gmail.com>
2018-03-17 16:48:04 +0300

    add extra url params to access with psa session

--- django_social_pill/templatetags/social_login.py
+++ django_social_pill/templatetags/social_login.py
@@ -1,3 +1,4 @@
+import urllib
 from django import template
 from django.shortcuts import reverse
 from django.conf import settings
@@ -5,9 +6,12 @@ from django.conf import settings
 register = template.Library()
 
 
-def construct_context(next_url, is_login, is_large):
+def construct_context(next_url, is_login, is_large, extra_login_params):
+    query_params = {'next': next_url}
+    if extra_login_params:
+        query_params.update(extra_login_params)
     context = {
-        'next': next_url,
+        'query_string': urllib.parse.urlencode(query_params),
         'is_login': is_login,
         'is_large': is_large
     }
@@ -15,33 +19,33 @@ def construct_context(next_url, is_login, is_large):
 
 
 @register.inclusion_tag('django_social_pill/github_login.html')
-def show_github_login(next_url, is_login, is_large):
-    return construct_context(next_url, is_login, is_large)
+def show_github_login(next_url, is_login, is_large, extra_login_params=None):
+    return construct_context(next_url, is_login, is_large, extra_login_params)
 
 
 @register.inclusion_tag('django_social_pill/vk_login.html')
-def show_vk_login(next_url, is_login, is_large):
-    return construct_context(next_url, is_login, is_large)
+def show_vk_login(next_url, is_login, is_large, extra_login_params=None):
+    return construct_context(next_url, is_login, is_large, extra_login_params)
 
 
 @register.inclusion_tag('django_social_pill/facebook_login.html')
-def show_facebook_login(next_url, is_login, is_large):
-    return construct_context(next_url, is_login, is_large)
+def show_facebook_login(next_url, is_login, is_large, extra_login_params=None):
+    return construct_context(next_url, is_login, is_large, extra_login_params)
 
 
 @register.inclusion_tag('django_social_pill/twitter_login.html')
-def show_twitter_login(next_url, is_login, is_large):
-    return construct_context(next_url, is_login, is_large)
+def show_twitter_login(next_url, is_login, is_large, extra_login_params=None):
+    return construct_context(next_url, is_login, is_large, extra_login_params)
 
 
 @register.inclusion_tag('django_social_pill/google_login.html')
-def show_google_login(next_url, is_login, is_large):
-    return construct_context(next_url, is_login, is_large)
+def show_google_login(next_url, is_login, is_large, extra_login_params=None):
+    return construct_context(next_url, is_login, is_large, extra_login_params)
 
 
 @register.inclusion_tag('django_social_pill/telegram_login.html')
 def show_telegram_login(button_size='large', ask_to_send_messages=False,
-                        show_user_photo=True, corner_radius=20, next=''):
+                        show_user_photo=True, corner_radius=20, next='', extra_login_params=None):
     allowed_button_sizes = ('large', 'medium', 'small')
     if button_size not in allowed_button_sizes:
         raise ValueError(
@@ -51,6 +55,11 @@ def show_telegram_login(button_size='large', ask_to_send_messages=False,
         raise ValueError('corner_radius can only be an integer')
     redirect_url = reverse('social:complete', kwargs={'backend': 'telegram'})
     bot_name = settings.SOCIAL_PILL_TELEGRAM_BOT_NAME
+
+    query_params = {'next': next}
+    if extra_login_params:
+        query_params.update(extra_login_params)
+
     return {
         'bot_name': bot_name,
         'redirect_url': redirect_url,
@@ -58,5 +67,5 @@ def show_telegram_login(button_size='large', ask_to_send_messages=False,
         'ask_to_send_messages': ask_to_send_messages,
         'show_user_photo': show_user_photo,
         'corner_radius': corner_radius,
-        'next': next,
+        'query_string': urllib.parse.urlencode(query_params),
     }
